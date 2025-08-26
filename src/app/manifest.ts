a2cf99181e481ff0b5f72b1a2798d98d9f148e76
Shanoof <shanoof.001@gmail.com>
2025-08-26 17:33:14 +0530

    feat:convert to pwa

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,24 +2,23 @@ import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Next.js PWA",
-    short_name: "NextPWA",
-    description: "A Progressive Web App built with Next.js",
+    name: "Abacus",
+    short_name: "Abacus",
+    description: "A Personal Finance Tracker.",
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#000000",
+    theme_color: "#ffffff",
     icons: [
-      {
-        src: "/logo.svg",
-        sizes: "192x192",
-        type: "image/svg+xml",
-      },
-      {
-        src: "/logo.svg",
-        sizes: "512x512",
-        type: "image/svg+xml",
-      },
+      { src: "/icon-48.png", sizes: "48x48", type: "image/png" },
+      { src: "/icon-72.png", sizes: "72x72", type: "image/png" },
+      { src: "/icon-96.png", sizes: "96x96", type: "image/png" },
+      { src: "/icon-128.png", sizes: "128x128", type: "image/png" },
+      { src: "/icon-144.png", sizes: "144x144", type: "image/png" },
+      { src: "/icon-152.png", sizes: "152x152", type: "image/png" },
+      { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { src: "/icon-384.png", sizes: "384x384", type: "image/png" },
+      { src: "/icon-512.png", sizes: "512x512", type: "image/png" },
     ],
   };
 }
